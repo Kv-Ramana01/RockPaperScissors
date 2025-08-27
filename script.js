42c3078b79016bf4f248ff2858dd7d477fa5adab
Kv-Ramana01 <145438290+Kv-Ramana01@users.noreply.github.com>
2025-08-27 12:53:39 +0530

    added js file. implemented basic functionality of rock paper scissors game.

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function getComputerChoice(string) {
+    let num = Math.floor(Math.random() * 3);
+
+    if(num === 0) {
+        return "rock";
+    } else if(num === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }    
+}
+
+let playerScore = 0; 
+let computerScore = 0;
+let rounds = parseInt(prompt("How many rounds do you want to play?"));
+
+
+while(rounds > 0){
+    let playerChoice = prompt("rock, paper, or scissors?").toLowerCase();
+    let computerChoice = getComputerChoice();
+
+    if(playerChoice === computerChoice){
+        alert("It's a tie! Both chose " + playerChoice);
+    }else if((playerChoice === "rock" && computerChoice === "scissors") || 
+             (playerChoice === "paper" && computerChoice === "rock") || 
+             (playerChoice === "scissors" && computerChoice === "paper")){
+        alert("You win! " + playerChoice + " beats " + computerChoice);
+        playerScore++;
+    }else {
+        alert("You lose! " + computerChoice + " beats " + playerChoice);
+        computerScore++;
+    }
+
+    rounds--;
+    console.log(`Round: ${rounds}, Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+    // choice = prompt("Do you want to play again? (yes/no)").toLowerCase();
+}
+
+if(playerScore > computerScore){
+    alert("Game over! You win with a score of " + playerScore + " to " + computerScore);
+}else if(computerScore > playerScore){
+    alert("Game over! You lose with a score of " + playerScore + " to " + computerScore);
+}else{
+    alert("Game over! It's a tie with a score of " + playerScore + " to " + computerScore);
+}
+
